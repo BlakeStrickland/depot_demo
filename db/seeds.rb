a4e8b2cc459d20aee36a091126b904a77f172194
Blake Strickland <Stricklandb02@gmail.com>
2016-06-01 19:50:37 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,19 @@ Product.create!(title: "Programming Ruby 2.3",
 description: %{<p>
 Ruby is the fastest growing and most exciting dynamic language out there. if you need to get working programs delivered fast, you should probably add Ruby to your toolbox.
 </p>},
-image_url: "logo.jpg",
+image_url: "ruby.png",
+price: 49.95)
+
+Product.create!(title: "DevOps in Practice",
+description: %{<p>
+Super sweet cover and a good dipiction of what I am currently working on.
+</p>},
+image_url: "devops.jpg",
+price: 49.95)
+
+Product.create!(title: "Rapid Android Development",
+description: %{<p>
+Next stop.
+</p>},
+image_url: "android.png",
 price: 49.95)
