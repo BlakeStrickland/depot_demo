0ca1e26d17e4d81555ba8e843ff85aa07e0670b4
Blake Strickland <Stricklandb02@gmail.com>
2016-06-02 15:46:59 -0400

    Add integration test

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -5,15 +5,16 @@ class OrderNotifierTest < ActionMailer::TestCase
     mail = OrderNotifier.received(orders(:one))
     assert_equal "Depot Conformation Email", mail.subject
     assert_equal ["dave@example.com"], mail.to
-    assert_equal ["depot@example.com"], mail.from
+    assert_equal ["StricklandB02@gmail.com"], mail.from
+    assert_match /Depot Conformation Email/, mail.subject
   end
 
   test "shipped" do
     mail = OrderNotifier.shipped(orders(:one))
     assert_equal "Depot Conformation Email", mail.subject
     assert_equal ["dave@example.com"], mail.to
-    assert_equal ["depot@example.com"], mail.from
-
+    assert_equal ["StricklandB02@gmail.com"], mail.from
+    assert_match /Depot Conformation Email/, mail.subject
   end
 
 end
